f2fa77e65e222deb65645036f7360b840a723c82
parmesh777 <parmesh.kandregula@gmail.com>
2020-12-01 15:46:51 +0530

    without all validation

--- client/src/components/UserForm.js
+++ client/src/components/UserForm.js
@@ -1,153 +1,184 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 import "../App.css";
 
 export default class CreateUser extends Component {
-    state = {
-        firstName:'',
-        lastName:'',
-        age:0,
-        gender:'',
-        error:'',
-        visible: true,
-        users:[]
-    }
- 
+  state = {
+    firstName: "",
+    lastName: "",
+    age: 0,
+    gender: "",
+    error: "",
+    visible: true,
+    users: [],
+  };
 
-componentDidMount() {
-    axios.get('/api/users')
-    .then(response => {
-        if(response.data.length > 0) {
-            this.setState({
-                users: response.data.map(user => user.firstName),
-                
-            })
-        }
-    })
-}
-
-validate= () => {
-    let error='';
-
-      if(!this.state.firstName || !this.state.lastName || !this.state.age){
-        error="can't be Blank";
-    }
-      if(error){
-          this.setState({error});
-          return false;
+  componentDidMount() {
+    axios.get("/api/users").then((response) => {
+      if (response.data.length > 0) {
+        this.setState({
+          users: response.data.map((user) => user.firstName),
+        });
       }
-      return true;
+    });
+  }
 
-}
-    onChange = (e) =>{
-        this.setState({
-            [e.target.name]: e.target.value
-        })
+  validate = () => {
+    let error = "";
+
+    if (!this.state.firstName || !this.state.lastName || !this.state.age) {
+      error = "can't be Blank";
     }
-
-    handleSubmit = (e) =>{
-        e.preventDefault();
-        const config ={
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        };
-
-        const userData = {
-            firstName:this.state.firstName,
-            lastName:this.state.lastName,
-            age:this.state.age,
-            gender:this.state.gender
-        };
-    
-        
-       const isValid = this.validate();
-        if(isValid){
-            axios.post('/api/users', userData, config)
-            .then(res => console.log(res.data));
-           window.location="/";
-    
-        this.setState({
-            firstName:'',
-            lastName:'',
-            age:0,
-            gender:''
-        })
+    if (error) {
+      this.setState({ error });
+      return false;
     }
-        }
-        
+    return true;
+  };
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-        
-    render() {
-        return (
-            <div className="container">
-                <div className="container" style={{textAlign:"center"}}>
-                <h1>User Entry Details</h1>
-    <button className="btn btn-primary" onClick={() => {this.setState({
-        visible:false
-    })}}>Add Details</button>
-    </div>
-                    <br/>
-                    <br/>
-                    {this.state.visible ? null :
-                <form onSubmit={this.handleSubmit} className="contain">
-                    
-                    <div className="form-group">
-                    
-                        <label>Firstname: </label>
-                        <br/>
-                        <input type="text"
-                        placeholder="Enter firstname"
-                        name="firstName"
-                        className="form-group"
-                        value={this.state.firstName}
-                        onChange={this.onChange}/>
-                        <div style={{ fontSize: 12, color: "red"}}>{this.state.error}</div>
-                        
-                    </div>
-                    
-                    <div className="form-group">
-                        <label>Lastname: </label>
-                        <br/>
-                        <input type="text"
-                        placeholder="Enter lastname"
-                        name="lastName"
-                        className="form-group"
-                        value={this.state.lastName}
-                        onChange={this.onChange}/>
-                        <div style={{ fontSize: 12, color: "red"}}>{this.state.error}</div>
-                    </div>
-                    
-                    <div className="form-group">
-                        <label>Age: </label>
-                        <br/>
-                        <input type="number"
-                        name="age"
-                        className="form-group"
-                        value={this.state.age}
-                        onChange={this.onChange}/>
-                        <div style={{ fontSize: 12, color: "red"}}>{this.state.error}</div>
-                    </div>
-                    
-                    <div className="form-group">
-                        <label>Gender:
-                            <br/>
-                        <input type="radio" value="male" name="gender" checked={this.state.gender ==="male"} onChange={this.onChange}/>Male 
-                        <br/>
-                        
-                        <input type="radio" value="female" name="gender" checked={this.state.gender ==="female"} onChange={this.onChange}/>Female
-                        </label>
-                        <div style={{ fontSize: 12, color: "red"}}>{this.state.error}</div>
-                         </div>
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-                         <div className="form-group">
-                            <input type="submit" value="CreateUser" className="btn btn-primary" />
-                         </div>
-                </form>
+    const userData = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      age: this.state.age,
+      gender: this.state.gender,
+    };
+
+    const isValid = this.validate();
+    if (isValid) {
+      axios
+        .post("/api/users", userData, config)
+        .then((res) => console.log(res.data));
+      window.location = "/";
+
+      this.setState({
+        firstName: "",
+        lastName: "",
+        age: 0,
+        gender: "",
+      });
     }
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="container" style={{ textAlign: "center" }}>
+          <h1>User Entry Details</h1>
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              this.setState({
+                visible: !this.state.visible,
+              });
+            }}
+          >
+            Add Details
+          </button>
+        </div>
+        <br />
+        <br />
+        {this.state.visible ? null : (
+          <form onSubmit={this.handleSubmit} className="contain">
+            <div className="form-group">
+              <label>Firstname: </label>
+              <br />
+              <input
+                type="text"
+                placeholder="Enter firstname"
+                name="firstName"
+                className="form-group"
+                value={this.state.firstName}
+                onChange={this.onChange}
+              />
+              <div style={{ fontSize: 14, color: "red" }}>
+                {this.state.error}
+              </div>
             </div>
-        )
-    }
+
+            <div className="form-group">
+              <label>Lastname: </label>
+              <br />
+              <input
+                type="text"
+                placeholder="Enter lastname"
+                name="lastName"
+                className="form-group"
+                value={this.state.lastName}
+                onChange={this.onChange}
+              />
+              <div style={{ fontSize: 14, color: "red" }}>
+                {this.state.error}
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label>Age: </label>
+              <br />
+              <input
+                type="number"
+                name="age"
+                className="form-group"
+                value={this.state.age}
+                onChange={this.onChange}
+              />
+              <div style={{ fontSize: 14, color: "red" }}>
+                {this.state.error}
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label>
+                Gender:
+                <br />
+                <input
+                  type="radio"
+                  value="male"
+                  name="gender"
+                  checked={this.state.gender === "male"}
+                  onChange={this.onChange}
+                />
+                Male
+                <br />
+                <input
+                  type="radio"
+                  value="female"
+                  name="gender"
+                  checked={this.state.gender === "female"}
+                  onChange={this.onChange}
+                />
+                Female
+              </label>
+              <div style={{ fontSize: 14, color: "red" }}>
+                {this.state.error}
+              </div>
+            </div>
+
+            <div className="form-group ">
+              <input
+                type="submit"
+                value="Submit"
+                className="btn btn-primary  "
+                style={{
+                  minWidth: "200%",
+                }}
+              />
+            </div>
+          </form>
+        )}
+      </div>
+    );
+  }
 }
-
-
--- client/src/components/UsersList.js
+++ client/src/components/UsersList.js
@@ -1,82 +1,85 @@
-import axios from 'axios';
-import React, { Component } from 'react';
+import axios from "axios";
+import React, { Component } from "react";
 import UserForm from "./UserForm";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../App.css";
 
 class UsersList extends Component {
+  state = {
+    users: [],
+  };
 
-    state={
-        users:[]
-    };
-
-    
-
-
-getUsers(){
-    axios.get('/api/users')
-    .then(res => {
-        this.setState({users: res.data})
-    })
-    .catch((error) => {
+  getUsers() {
+    axios
+      .get("/api/users")
+      .then((res) => {
+        this.setState({ users: res.data });
+      })
+      .catch((error) => {
         console.log(error);
-    })
-}
+      });
+  }
 
+  componentDidMount() {
+    this.getUsers();
+  }
+  deleteUser = (id) => {
+    axios.delete("/api/users/" + id).then((res) => console.log(res.data));
 
-componentDidMount() {
-        this.getUsers();
-}
-    deleteUser = (id) => {
-        
-        axios.delete('/api/users/'+id)
-        .then(res => console.log(res.data));
+    this.setState({
+      users: this.state.users.filter((el) => el._id !== id),
+    });
+  };
 
-        this.setState({
-            users: this.state.users.filter(el => el._id !== id)
-        })
-    }
-
-    
-
-     
-    render(){
+  render() {
     return (
-        <div> 
-            <UserForm />
-                <table className="table table-striped">
-                  <thead className="tableHover">
-                      <tr>
-                          <th>Firstname</th>
-                          <th>Lastname</th>
-                          <th>Age</th>
-                          <th>Gender</th>
-                          <th>Edit</th>
-                          <th>Delete</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      { this.state.users.map(currentUser => {
-            
-              return(  <tr key={currentUser._id}>
-                    <td>{currentUser.firstName}</td>
-                    <td>{currentUser.lastName}</td>
-                    <td>{currentUser.age}</td>
-                    <td>{currentUser.gender}</td>
-                    <td>
-                        <Link to={"/edit/"+currentUser._id}  className="btn btn-info">Edit</Link>
-                    </td>
-                    <td>
-                        <button onClick={() => this.deleteUser(currentUser._id)} className="btn btn-danger">Delete</button>
-                    </td>
-                </tr>)
-            
-        }) }
-                  </tbody>
-                </table>
-        </div>
-    )
-    }
+      <div>
+        <UserForm />
+        <table className="table table-striped">
+          <thead className="tableHover">
+            <tr>
+              <th>Sr no.</th>
+              <th>Firstname</th>
+              <th>Lastname</th>
+              <th>Age</th>
+              <th>Gender</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.users.map((currentUser, index) => {
+              return (
+                <tr key={currentUser._id}>
+                  <td>{index}</td>
+                  <td>{currentUser.firstName}</td>
+                  <td>{currentUser.lastName}</td>
+                  <td>{currentUser.age}</td>
+                  <td>{currentUser.gender}</td>
+                  <td>
+                    <Link
+                      to={"/edit/" + currentUser._id}
+                      className="btn btn-info"
+                    >
+                      Edit
+                    </Link>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => this.deleteUser(currentUser._id)}
+                      className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default UsersList;
